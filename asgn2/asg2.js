0318734c845d34404457dad3e35fa4953c3ed96c
ihinan6 <inath@ucsc.edu>
2025-04-28 13:46:13 -0700

    commit

--- asgn2/asg2.js
+++ asgn2/asg2.js
@@ -6,30 +6,32 @@ let canvas;
 let gl;
 let a_Position;
 let u_FragColor;
-let u_Size;
+//let u_Size;
 let u_ModelMatrix;
 let u_GlobalRotateMatrix;
 let g_selectedColor = [1,1,1,1]
-let g_selectedSize = 5;
-let g_selectedType = POINT;
+//let g_selectedSize = 5;
+//let g_selectedType = POINT;
 let g_selectedSides = 3;
-var g_selectedBook;
+//var g_selectedBook;
 var g_shapesList = [];
 
 let g_globalAngle = 0;
 let g_yellowAngle = 0;
 let g_magentaAngle = 0;
+let g_beakAngle = 0;
 let g_yellowAnimation = false;
 let g_magentaAnimation = false;
+let g_beakAnimation = false;
 
 var VSHADER_SOURCE =
 	    'attribute vec4 a_Position;\n' +
-	    'uniform float u_Size;\n' +
+	    //'uniform float u_Size;\n' +
 	    'uniform mat4 u_ModelMatrix;\n' +
 	    'uniform mat4 u_GlobalRotateMatrix;\n' +
 	    'void main() {\n' +
-	    '  gl_Position = a_Position;\n' +
-	    '  gl_PointSize = u_Size;\n' +
+	    //'  gl_Position = a_Position;\n' +
+	   // '  gl_PointSize = u_Size;\n' +
             '  gl_Position = u_GlobalRotateMatrix * u_ModelMatrix * a_Position;' +	
 	    '}\n';
 	    /**	
@@ -52,13 +54,11 @@ function setUpWebGL(){
 		console.log('Failed to get <canvas> element');
 		return;
 	}
-
 	gl = getWebGLContext(canvas);
 	if(!gl){
 		console.log('Failed to get rendering context for WebGL');
 		return;
 	}
-
 	gl.enable(gl.DEPTH_TEST);
 }
 
@@ -66,9 +66,9 @@ function setUpWebGL(){
 function addActionForHtmlUI(){
 	document.getElementById('clear').onclick = function(){
 		g_shapesList = [];
-		renderAllShapes();
+		renderScene();
 	};
-
+/**
 	document.getElementById('pointButton').onclick = function(){
 		g_selectedType = POINT;
 	};
@@ -106,7 +106,7 @@ function addActionForHtmlUI(){
 		drawPicture();
 	};
 
-
+**/
 	// new stuff
 	document.getElementById('animationYellowOffButton').onclick = function() {g_yellowAnimation = false;};
 	document.getElementById('animationYellowOnButton').onclick = function() {g_yellowAnimation = true;};
@@ -114,30 +114,40 @@ function addActionForHtmlUI(){
 	document.getElementById('animationMagentaOffButton').onclick = function() {g_magentaAnimation = false;};
 	document.getElementById('animationMagentaOnButton').onclick = function() {g_magentaAnimation = true;};
 
+	document.getElementById('animationBeakOffButton').onclick = function() {g_beakAnimation = false;};
+	document.getElementById('animationBeakOnButton').onclick = function() {g_beakAnimation = true;};
+
+
 
 	// angle slider event
 	document.getElementById('angleSlide').addEventListener('mousemove',function(){
 		g_globalAngle = this.value;
-		renderAllShapes();
+		renderScene();
 	});
 
 	document.getElementById('yellowSlide').addEventListener('mousemove', function(){
 		g_yellowAngle = this.value;
-		renderAllShapes();
+		renderScene();
 
 	});
 
 	document.getElementById('magentaSlide').addEventListener('mousemove', function(){
 		g_magentaAngle = this.value;
-		renderAllShapes();
+		renderScene();
 	});
+
+	document.getElementById('beakSlide').addEventListener('mousemove', function(){
+		g_beakAngle = this.value;
+		renderScene();
+	});
+
 }
 
 /**
 function addActionForHtmlUI(){
 	document.getElementByID('clear').onclick = function(){
 		g_shapesList = [];
-		renderAllShapes();
+		renderScene();
 	}
 	document.getElementById('pointButton').onclick = function(){
 		g_selectedType = POINT;
@@ -168,7 +178,7 @@ function main(){
 
 	gl.clearColor(0,0,0,1);
 	//gl.clear(gl.COLOR_BUFFER_BIT);
-	//renderAllShapes();
+	//renderScene();
 	requestAnimationFrame(tick);
 }
 
@@ -180,7 +190,7 @@ function tick(){
 	g_seconds = performance.now()/1000 - g_startTime;
 	//console.log(performance.now());
 	updateAnimationAngles();
-	renderAllShapes();
+	renderScene();
 	requestAnimationFrame(tick);
 }
 
@@ -241,10 +251,13 @@ function updateAnimationAngles(){
 	if(g_magentaAnimation){
 		g_magentaAngle = (45*Math.sin(3*g_seconds));
 	}
+	if(g_beakAnimation){
+		g_beakAngle = Math.abs((45*Math.sin(3*g_seconds)));
+	}
 }
 
 
-function renderAllShapes(){
+function renderScene(){
 
 	// check the time at the start of this function
 	var startTime = performance.now();
@@ -269,12 +282,64 @@ function renderAllShapes(){
 	
 	// draw the body cube
 	var body = new Cube();
-	body.color = [1,0,0,1];
-	body.matrix.translate(-.25,-.75,0.0);
+	body.color = [0.9,0.9,0.9,1];
+	body.matrix.setTranslate(-0.2,-0.2,-0.2);
 	body.matrix.rotate(-5,1,0,0);
-	body.matrix.scale(0.5,.3,.5);
+	body.matrix.scale(0.45,.35,.55);
 	body.render();
 
+	// head
+	var head = new Cube();
+	head.color = [0.85,0.85,0.85,1];
+	head.matrix.setTranslate(-0.075,0.05,-0.31);
+	head.matrix.rotate(-5,1,0,0);
+	head.matrix.scale(0.2,0.3,.15);
+	head.render();
+
+	// beak1
+	var beak1 = new Cube();
+	beak1.color = [1,0.7,0,1];
+	beak1.matrix.setTranslate(-0.075,0.2,-0.469);
+	beak1.matrix.rotate(-5,1,0,0);
+	beak1.matrix.scale(0.2,0.05,.15);
+	beak1.render();
+
+	// beak2
+	var beak2 = new Cube();
+	beak2.color = [1,0.6,0,1];
+	beak2.matrix.setTranslate(-0.075,0.21,-0.32);
+	beak2.matrix.rotate(175,1,0,0);
+	beak2.matrix.rotate(-g_beakAngle,1,0,0);
+	beak2.matrix.scale(0.2,0.05,0.15);
+	beak2.render();
+
+
+	// reye
+	var reye = new Cube();
+	reye.color = [0.1,0.1,0.1,1];
+	reye.matrix.setTranslate(-0.075,0.26,-0.35);
+	reye.matrix.rotate(-5,1,0,0);
+	reye.matrix.scale(0.05,0.05,0.02);
+	reye.render();
+
+	// leye
+	var leye = new Cube();
+	leye.color = [0.1,0.1,0.1,1];
+	leye.matrix.setTranslate(0.075,0.26,-0.35);
+	leye.matrix.rotate(-5,1,0,0);
+	leye.matrix.scale(0.05,0.05,.02);
+	leye.render();
+
+	// redthing
+	var redthing = new Cube();
+	redthing.color = [1,0.2,0,1];
+	redthing.matrix.setTranslate(-0.025,0.06,-0.35);
+	redthing.matrix.rotate(-5,1,0,0);
+	redthing.matrix.scale(0.1,0.1,.05);
+	redthing.render();
+
+
+/**
 	// draw left arm
 	var leftArm = new Cube();
 	leftArm.color = [1,1,0,1];
@@ -287,7 +352,7 @@ function renderAllShapes(){
 	}else{
 		leftArm.matrix.rotate(-g_yellowAngle,0,0,1);
 	}
-**/
+
 	var yellowCoordinatesMat = new Matrix4(leftArm.matrix);
 	leftArm.matrix.scale(0.25,.7,.5);
 	leftArm.matrix.translate(-.5,0,0);
@@ -305,6 +370,7 @@ function renderAllShapes(){
 //	box.matrix.rotate(-30,1,0,0);
 //	box.matrix.scale(.2,.4,.2);
 	box.render();
+**/
 
 	// check the time at the end of the function and show on webpage
 	var duration = performance.now() - startTime;
@@ -327,10 +393,10 @@ function click(ev){
 
 	point.position = [x,y];
 	point.color = g_selectedColor.slice();
-	point.size = g_selectedSize;
+//	point.size = g_selectedSize;
 	g_shapesList.push(point);
 
-	renderAllShapes();
+	renderScene();
 }
 
 
